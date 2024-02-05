9b1cf37e9fff2b656c3f01809c7e7ef8f077181d
Rupert Minnett <rminnett@gmail.com>
2024-02-05 13:43:02 -0600

    Add append to private contribution endpoint

--- src/v1/path/private.tests.ts
+++ src/v1/path/private.tests.ts
@@ -6,7 +6,7 @@ dotenv.config();
 jest.setTimeout(30000);
 const v = 'v1';
 
-describe(`FIESTA API ${v} Private Create/Update/Delete Tests`, () => {
+describe(`FIESTA API ${v} Private Create/Update/Append/Delete Tests`, () => {
 	let client: AxiosInstance;
 
 	beforeAll(async () => {
@@ -36,6 +36,66 @@ describe(`FIESTA API ${v} Private Create/Update/Delete Tests`, () => {
 		expect(createRes.status).toBe(201);
 		expect(createRes.data).toHaveProperty('id');
 		expect(createRes.data.id).toBeGreaterThanOrEqual(1);
+		
+        const deleteRes = await client.delete(
+			`/${v}/MagIC/private?id=${createRes.data.id}`,
+			{
+				auth: {
+					username: process.env.TEST_USERNAME,
+					password: process.env.TEST_PASSWORD,
+				},
+			}
+		);
+		expect(deleteRes.status).toBe(200);
+		expect(deleteRes.data).toHaveProperty('rowsDeleted');
+		expect(deleteRes.data.rowsDeleted).toEqual(1);
+	});
+
+	test(`PUT /${v}/MagIC/private`, async () => {
+        const updateRes = await client.put(
+			`/${v}/MagIC/private?id=19933`,
+			{ },
+			{
+				auth: {
+					username: process.env.TEST_USERNAME,
+					password: process.env.TEST_PASSWORD,
+				},
+			}
+        );
+		expect(updateRes.status).toBe(201);
+		expect(updateRes.data).toHaveProperty('id');
+        expect(updateRes.data.id).toBeGreaterThanOrEqual(1);
+	});
+
+	test(`POST /${v}/MagIC/private, PUT /${v}/MagIC/private and DELETE /${v}/MagIC/private`, async () => {
+		const createRes = await client.post(
+			`/${v}/MagIC/private`,
+			{},
+			{
+				auth: {
+					username: process.env.TEST_USERNAME,
+					password: process.env.TEST_PASSWORD,
+				},
+			}
+		);
+		expect(createRes.status).toBe(201);
+		expect(createRes.data).toHaveProperty('id');
+        expect(createRes.data.id).toBeGreaterThanOrEqual(1);
+        
+        const updateRes = await client.put(
+			`/${v}/MagIC/private`,
+			{ },
+			{
+				auth: {
+					username: process.env.TEST_USERNAME,
+					password: process.env.TEST_PASSWORD,
+				},
+			}
+        );
+		expect(updateRes.status).toBe(201);
+		expect(updateRes.data).toHaveProperty('id');
+        expect(updateRes.data.id).toBeGreaterThanOrEqual(1);
+
 		const deleteRes = await client.delete(
 			`/${v}/MagIC/private?id=${createRes.data.id}`,
 			{
--- src/v1/path/private.ts
+++ src/v1/path/private.ts
@@ -106,6 +106,7 @@ export default {
 				});
 			}
 			const doc = { contribution: {}, summary: {} };
+            const tables_not_processed = [];
 
 			// Parse the contribution
 			const { Parser: ParseContribution } = await import(
@@ -167,9 +168,137 @@ export default {
 
 			ctx.status = 202;
 			ctx.body = {
-				rowsAdded: 0,
-				rowsChanged: 0,
-				rowsDeleted: 0,
+                id: id,
+                tables_not_processed: tables_not_processed,
+				rows_added: 0,
+				rows_deleted: 0,
+			};
+		} catch (e) {
+			ctx.app.emit('error', e, ctx);
+			ctx.status = 500;
+			ctx.body = { errors: [{ message: e.message }] };
+		}
+	},
+	v1PrivateAppend: async (
+		c: OpenAPIContext,
+		ctx: Koa.Context
+	): Promise<void> => {
+		try {
+			const user = await esAuthenticate(ctx.headers.authorization);
+			if (user === false) {
+				ctx.body = {
+					errors: [{ message: 'Username or password is not recognized.' }],
+				};
+				ctx.status = 401;
+				return;
+			}
+			const contributor = user.handle ? `@${user.handle}` : `@user${user.id}`;
+			const { repository: repositories } = c.request.params;
+			const { id: ids } = c.request.query;
+			const repository: string =
+				repositories instanceof Array ? repositories[0] : repositories;
+			const prevID: string = ids instanceof Array ? ids[0] : ids;
+			const prevDoc = await esGetContribution({
+				repository,
+				id: prevID,
+			});
+			if (prevDoc === undefined) {
+				ctx.status = 202;
+				return;
+			}
+			let id: number = prevDoc['summary']['contribution']['id'];
+			const prevContributionSummary = prevDoc['summary']['contribution'];
+
+			// Check that the contribution can be updated.
+			const prevHandle = prevContributionSummary['contributor'];
+			const nextHandle = user.handle ? `@${user.handle}` : `@user${user.id}`;
+			if (prevHandle !== '@magic' && prevHandle !== nextHandle) {
+				ctx.body = {
+					errors: [
+						{
+							message: `The contribution with ID ${id} is owned by another contributor.`,
+						},
+					],
+				};
+				ctx.status = 401;
+				return;
+			}
+
+			// Create a new private contribution if this contribution is public.
+			if (prevContributionSummary['is_activated'] === 'true') {
+				id = await esCreatePrivate({
+					repository,
+					contributor,
+					contributorName: `${user.name.given} ${user.name.family}`,
+				});
+			}
+			const doc = { contribution: {}, summary: {} };
+            const tables_not_processed = [];
+
+			// Parse the contribution
+			const { Parser: ParseContribution } = await import(
+				'../libs/parse_contribution.js'
+			);
+			const parser = new ParseContribution({});
+			await parser.parsePromise({ text: c.request.body });
+			fs.writeFileSync(
+				`magic_doc_${id}_parser_ew.json`,
+				JSON.stringify(parser.errorsAndWarnings(), null, 2)
+			);
+
+			doc.contribution = parser.json;
+			doc.contribution['contribution'] =
+				prevDoc['contribution']['contribution'];
+			fs.writeFileSync(
+				`magic_doc_${id}_prev.json`,
+				JSON.stringify(doc, null, 2)
+			);
+			fs.writeFileSync(
+				`magic_doc_${id}_prev_summary.json`,
+				JSON.stringify(prevContributionSummary, null, 2)
+			);
+
+			// Summerize the contribution
+			const { Summarizer: SummarizeContribution } = await import(
+				'../libs/summarize_contribution.js'
+			);
+			const summarizer = new SummarizeContribution({});
+			await summarizer.preSummarizePromise(doc.contribution, {
+				summary: { contribution: prevContributionSummary },
+			});
+			fs.writeFileSync(
+				`magic_doc_${id}_presummarizer.json`,
+				JSON.stringify(summarizer.json, null, 2)
+			);
+			fs.writeFileSync(
+				`magic_doc_${id}_presummarizer_ew.json`,
+				JSON.stringify(summarizer.errorsAndWarnings(), null, 2)
+			);
+			doc.summary = summarizer.json.contribution.summary;
+			//await summarizer.summarizePromise(doc.contribution, { summary: { contribution: prevContributionSummary }});
+			//fs.writeFileSync(`magic_doc_${id}_summarizer.json`, JSON.stringify(summarizer.json, null, 2));
+			//fs.writeFileSync(`magic_doc_${id}_summarizer_ew.json`, JSON.stringify(summarizer.errorsAndWarnings(), null, 2));
+			//doc.summary = summarizer.json.contribution.summary;
+			console.log(prevContributionSummary);
+			fs.writeFileSync(
+				`magic_doc_${id}_after.json`,
+				JSON.stringify(doc, null, 2)
+			);
+
+			// Index with summary
+			await esReplacePrivate({
+				repository,
+				contributor,
+				id,
+				doc,
+			});
+
+			ctx.status = 202;
+            ctx.body = {
+                id: id,
+                tables_not_processed: tables_not_processed,
+				rows_added: 0,
+				rows_deleted: 0,
 			};
 		} catch (e) {
 			ctx.app.emit('error', e, ctx);
